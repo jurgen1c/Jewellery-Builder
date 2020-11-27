5e25790e4e6e3cc2a4c889aea4a2ff10356ea729
jurgen1c <jurgen1c@gmail.com>
2020-11-27 17:56:49 -0600

    Add template, base, stonecolor select & stone drag and drop

--- Assets/JS/builder.js
+++ Assets/JS/builder.js
@@ -1,4 +1,5 @@
 let scontainer = document.querySelector('.temp-wrap');
+let tWrap = document.querySelector('.build-wrap');
 
 function createStone() {
   let wrap = document.createElement('div');
@@ -9,29 +10,59 @@ function createStone() {
   scontainer.appendChild(wrap);
 }
 
-/* function onDragStart(event) {
-
-  event.dataTransfer.setData('text/plain', event.target.id);
-
-  event
-    .currentTarget
-    .style
-    .background = 'yellow';
+function createTemplate(tvalue) {
+  let count = 0;
+  if(tvalue == 'one-p') {
+    while (tWrap.firstChild) {
+      tWrap.firstChild.remove();
+    }
+    let stone = document.createElement('div');
+    stone.setAttribute('class', 'stone-wrap1');
+    stone.setAttribute('ondrop', 'drop(event)');
+    stone.setAttribute('ondragover', 'allowDrop(event)');
+    tWrap.appendChild(stone);
+  } else if(tvalue == 'three-p'){
+    while (tWrap.firstChild) {
+      tWrap.firstChild.remove();
+    }
+    count = 3;
+    for(let i = 1; i <= count; i++){
+      let stone = document.createElement('div');
+      stone.setAttribute('class', `stone-wrap${i}`);
+      stone.setAttribute('ondrop', 'drop(event)');
+      stone.setAttribute('ondragover', 'allowDrop(event)');
+      tWrap.appendChild(stone);
+    }
+  }else if(tvalue == 'seven-p'){
+    while (tWrap.firstChild) {
+      tWrap.firstChild.remove();
+    }
+    count = 7;
+    for(let i = 1; i <= count; i++){
+      let stone = document.createElement('div');
+      stone.setAttribute('class', `stone-wrap${i}`);
+      stone.setAttribute('ondrop', 'drop(event)');
+      stone.setAttribute('ondragover', 'allowDrop(event)');
+      tWrap.appendChild(stone);
+    }
+  }
+  localStorage.setItem('tWrap', JSON.stringify(tWrap));
+  //const saveW = JSON.parse(localStorage.getItem('tWrap'));
 }
 
-function onDragOver(event) {
-  event.preventDefault();
+function selectBase(svalue){
+
+  tWrap.style.backgroundImage = `url(./Assets/Images/${svalue}.png)`;
+  /* if(svalue == 'pearls'){
+    webpacker import
+    css loader
+    tWrap.style.background = "url('../Images/pearls.png')";
+  }else if(svalue == 'silver'){
+    tWrap.style.background = "url('../Images/silver.png')";
+  }else if(svalue == 'gold'){
+    tWrap.style.background = "url('../Images/gold.png')";
+  } */
 }
-
-function onDrop(event) {
-  const id = event.dataTransfer.getData('text');
-  
-  const draggableElement = document.getElementById(id);
-  const dropzone = event.target;
-  dropzone.appendChild(draggableElement);
-
-  event.dataTransfer.clearData();
-} */
 
 function allowDrop(ev) {
   ev.preventDefault();
@@ -58,5 +89,29 @@ function colourFunction(id, color) {
 }
 
 function shapeFunction() {
-  
+
 }
+
+/* function onDragStart(event) {
+
+  event.dataTransfer.setData('text/plain', event.target.id);
+
+  event
+    .currentTarget
+    .style
+    .background = 'yellow';
+}
+
+function onDragOver(event) {
+  event.preventDefault();
+}
+
+function onDrop(event) {
+  const id = event.dataTransfer.getData('text');
+  
+  const draggableElement = document.getElementById(id);
+  const dropzone = event.target;
+  dropzone.appendChild(draggableElement);
+
+  event.dataTransfer.clearData();
+} */
---
+++ README.md
@@ -0,0 +1,51 @@
+# Jewelllery Builder
+
+> In this repo you will find a very basic version of the game Space Invaders. Feel free to clone the repo and try it out, let me know what you think and any sugestions you might have. 
+Note: This is a project for fun so implementations will be long winded.
+
+![](./Assets/images/home_screenshot.png)
+
+## Built With
+
+- Python
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Install
+
+Clone the project by typing ```https://github.com/jurgen1c/Space-Invaders-Custom.git```
+
+### Prerequisites
+
+Python 3
+
+### Usage
+
+In your terminal navigate to project directory and run command python3 main.py
+
+## Author
+
+👤 **Jurgen Clausen Gutierrez**
+
+- Github: [@jurgen1c](https://github.com/jurgen1c)
+- LinkedIn: [jurgen-clausen](https://www.linkedin.com/in/jurgen-clausen-2740061a9/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+Codding with Tim
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
